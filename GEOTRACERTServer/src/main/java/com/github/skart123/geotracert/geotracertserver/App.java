75f1ac4c1b94e46733a81ea7f19f4993518082af
Art <art.enikoff@gmail.com>
2013-11-18 21:03:11 +0400

    1). Changed name for "web folder" to "webFolder" in GEOTRACERTClient folder. @Loryngoth are u mad to give names with whitespaces?!))) 2). Folder with SRS sources are now complitely moved to SRS folder. !!!!!!!!!!! 3). Implemented Protocol.FILE feature - now our server can give static pages !!!!!!!!!!!

--- GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/App.java
+++ GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/App.java
@@ -14,11 +14,14 @@ import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Restlet;
 import org.restlet.data.Protocol;
+import org.restlet.resource.Directory;
 import org.restlet.routing.Router;
 
 
-public class App extends Application{
 
+public class App extends Application{
+     public static final String ROOT_URI = "file:///C:/Users/art/Documents/GitHub/temp2/GEOTRACERT/SRS/result/pdf/SRS_GEOTRACERT.pdf"; 
+     
       /**
      * Creates a root Restlet that will receive all incoming calls.
      */
@@ -27,11 +30,32 @@ public class App extends Application{
         Component component = new Component();
 
         // Add a new HTTP server listening on port 8182.
-        component.getServers().add(Protocol.HTTP, 8182);
+       /* component.getServers().add(Protocol.HTTP, 8182);
 
         // Attach the sample application.
-        component.getDefaultHost().attach("/firstSteps", new App());
-
+        component.getDefaultHost().attach("/firstSteps", new App());*/
+        
+        
+        
+        component.getServers().add(Protocol.HTTP, 8182);  
+        component.getClients().add(Protocol.FILE);  
+        // Create an application 
+        
+        Application application = new Application() {  
+            @Override  
+            public Restlet createInboundRoot() {  
+                    return new Directory(getContext(), ROOT_URI);  
+            }  
+        }; 
+        
+        
+        
+        // Attach the application to the component and 
+       // component.getDefaultHost().attach("/trace2",new App());  
+        
+        component.getDefaultHost().attach("",application);  
+        
+        component.getDefaultHost().attach("/hello",new App());
         // Start the component.
         component.start();
     }
@@ -42,7 +66,7 @@ public class App extends Application{
         Router router = new Router(getContext());
 
         // Defines only one route
-        router.attach("/hello", HelloWorldResource.class);
+        router.attach("/hello",HelloWorldResource.class);
 
         return router;
     }
