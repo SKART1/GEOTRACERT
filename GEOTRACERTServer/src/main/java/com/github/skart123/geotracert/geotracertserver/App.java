b6c74ff3e328a56e6847526f99a1c71a6e3bff7e
Art <art.enikoff@gmail.com>
2013-11-19 04:57:37 +0400

    The main web page displayed as "Directory" resource. Participate isn solution of #40 But still we have only absolute path in links. Maybe we can fix by recieving absolute path but on runtime? In code? closes

--- GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/App.java
+++ GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/App.java
@@ -14,14 +14,15 @@ import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Restlet;
 import org.restlet.data.Protocol;
+import org.restlet.data.Reference;
 import org.restlet.resource.Directory;
 import org.restlet.routing.Router;
 
 
 
 public class App extends Application{
-     public static final String ROOT_URI = "file:///C:/Users/art/Documents/GitHub/temp2/GEOTRACERT/SRS/result/pdf/SRS_GEOTRACERT.pdf"; 
-     
+     public static final String ROOT_URI = "file:///C:/Users/art/Documents/GitHub/temp8/GEOTRACERT/GEOTRACERTServer/target/clientFolder/"; 
+     public static final String ROOT_URI2 = "file:///C:/Users/art/Documents/GitHub/temp8/GEOTRACERT/GEOTRACERTServer/target/clientFolder/"; 
       /**
      * Creates a root Restlet that will receive all incoming calls.
      */
@@ -38,13 +39,21 @@ public class App extends Application{
         
         
         component.getServers().add(Protocol.HTTP, 8182);  
-        component.getClients().add(Protocol.FILE);  
+        component.getClients().add(Protocol.FILE );  
         // Create an application 
         
         Application application = new Application() {  
             @Override  
             public Restlet createInboundRoot() {  
-                    return new Directory(getContext(), ROOT_URI);  
+                //Конструктор ресурса
+                     Directory temp= new Directory(getContext(), ROOT_URI); 
+                    // Reference temp2=temp.getRootRef();
+                     //temp.s
+                    // temp2.setBaseRef(ROOT_URI2);
+                    // temp.setRootRef(temp2);
+                   //  temp2=temp.getRootRef();
+                     return temp;
+                
             }  
         }; 
         
@@ -56,6 +65,12 @@ public class App extends Application{
         component.getDefaultHost().attach("",application);  
         
         component.getDefaultHost().attach("/hello",new App());
+        
+     //    component.getDefaultHost().attach("/",
+    //                                  new Redirector(component.getContext().createChildContext(),
+      //                                               " {rh}/C:/Users/art/Documents/GitHub/temp8/GEOTRACERT/GEOTRACERTServer/target/clientFolder/index.html",
+      //                                               Redirector.MODE_CLIENT_SEE_OTHER));
+        
         // Start the component.
         component.start();
     }
@@ -67,7 +82,11 @@ public class App extends Application{
 
         // Defines only one route
         router.attach("/hello",HelloWorldResource.class);
-
+ 
+    // Print the requested URI path  
+        
+  
+     
         return router;
     }
 
