686a3c434762f0c26175f111780b13e6dbffeef6
Art <art.enikoff@gmail.com>
2013-11-19 20:47:04 +0400

    Added relative path support. temporary paths are written to console to check on linux

--- GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/App.java
+++ GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/App.java
@@ -10,6 +10,9 @@ package com.github.skart123.geotracert.geotracertserver;
 
 
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Restlet;
@@ -21,48 +24,55 @@ import org.restlet.routing.Router;
 
 
 
-public class App extends Application{
-     public static final String ROOT_URI = "file:///C:/Users/art/Documents/GitHub/temp8/GEOTRACERT/GEOTRACERTServer/target/clientFolder/"; 
-     public static final String ROOT_URI2 = "file:///C:/Users/art/Documents/GitHub/temp8/GEOTRACERT/GEOTRACERTServer/target/clientFolder/"; 
-      /**
+
+public class App extends Application
+{    
+   static final String pathToIndexBase1 = App.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+    static String pathToIndexBaseCorrect1; 
+   
+   static final File parentDir = new File(new File("").getAbsolutePath());
+   static final String pathToIndexBase2=parentDir.getAbsolutePath();
+   static String pathToIndexBaseCorrect2;
+
+     /**
      * Creates a root Restlet that will receive all incoming calls.
      */
      public static void main(String[] args) throws Exception {
-        // Create a new Component.
+
+            
+        pathToIndexBaseCorrect1="file:////"+pathToIndexBase1.substring(0, pathToIndexBase1.indexOf("target")+6)+"/clientFolder";
+       // "file:///"  /*File.separator+ File.separator*/+ROOT_URI4+"/clientFolder"
+        pathToIndexBaseCorrect2="file:\\\\"+pathToIndexBase2.replaceAll("/", File.separator)+"\\target\\clientFolder";
+          // Create a new Component.     
         Component component = new Component();
-
-        // Add a new HTTP server listening on port 8182.
-       /* component.getServers().add(Protocol.HTTP, 8182);
-
-        // Attach the sample application.
-        component.getDefaultHost().attach("/firstSteps", new App());*/
+       
+       
+        
+        System.out.println("App.class.getProtectionDomain().getCodeSource().getLocation().getPath()"); 
+        System.out.println(pathToIndexBaseCorrect1);
+        System.out.println("File");
+        System.out.println(pathToIndexBaseCorrect2);       
+     
         
         
         
         component.getServers().add(Protocol.HTTP, 8182);  
         component.getClients().add(Protocol.FILE );  
-        // Create an application 
         
+        // Create an application         
         Application application = new Application() {  
             @Override  
             public Restlet createInboundRoot() {  
-                //Конструктор ресурса
-                     Directory temp= new Directory(getContext(), ROOT_URI); 
-                    // Reference temp2=temp.getRootRef();
-                     //temp.s
-                    // temp2.setBaseRef(ROOT_URI2);
-                    // temp.setRootRef(temp2);
-                   //  temp2=temp.getRootRef();
-                     return temp;
-                
+                //Конструктор ресурса                 
+                Directory temp = new Directory(getContext(), pathToIndexBaseCorrect1);
+                return temp;         
             }  
         }; 
         
         
         
-        // Attach the application to the component and 
-       // component.getDefaultHost().attach("/trace2",new App());  
-        
+        // Attach the application to the component 
+        //main page: index.html
         component.getDefaultHost().attach("/index.html",application);  
         
         component.getDefaultHost().attach("/hello",new App());
@@ -82,15 +92,8 @@ public class App extends Application{
         Router router = new Router(getContext());
 
         // Defines only one route
-        router.attach("/hello",HelloWorldResource.class);
- 
-    // Print the requested URI path  
-        
-  
-     
+        router.attach("/hello",HelloWorldResource.class);    
         return router;
-    }
-
-    
+    }   
 }
 
