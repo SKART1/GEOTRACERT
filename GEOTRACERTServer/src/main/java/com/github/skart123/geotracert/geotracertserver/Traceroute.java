ce6759174b821e36d917b9669f94d44df3b95feb
dm-kiselev <dima_kiselev_spb@mail.ru>
2013-12-14 00:36:19 +0400

    improved parsing data from traceroute with applying regexp

--- GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/Traceroute.java
+++ GEOTRACERTServer/src/main/java/com/github/skart123/geotracert/geotracertserver/Traceroute.java
@@ -6,13 +6,17 @@ package com.github.skart123.geotracert.geotracertserver;
 
 /**
  *
- * @author Goko
+ * @authors
+ * Goko
+ * dm-kiselev
  */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.lang.SystemUtils;
 
 /**
@@ -20,150 +24,153 @@ import org.apache.commons.lang.SystemUtils;
  */
 public class Traceroute {
 
-    /*
-     * runtime.
-     */
-    private Runtime run;
-    private int OSType;
-    /**
-     * Иницилизация runtime для того команды работали
-     */
-    public Traceroute() throws Exception {
-        if(SystemUtils.IS_OS_WINDOWS==true)////для Windows tracert
-        {
-            OSType=0;
-        } 
-        else if(SystemUtils.IS_OS_UNIX==true)//для linux
-        {
-             OSType=1; 
-        }
-        else
-        {    
-            OSType=3;
-            throw new Exception("Unknown operating system");
-        }
-        run = Runtime.getRuntime();
-    }
+	/*
+	 * runtime.
+	 */
+	private Runtime run;
+	private int OSType;
 
-    /**
-     * Вызов команду traceroute, и возврашает хопы в виде list->
-     * TracerouteItems.
-     *
-     * @param destination пункт назначения hostname или IP
-     * @return list of TracerouteItems
-     */
-    public ArrayList<TracerouteItem> traceroute(String destination) {
-        /*
-         * Это будет наш результат.
-         */
-        ArrayList<TracerouteItem> result = new ArrayList<TracerouteItem>();
-        boolean firstLineFlag=true;
-        /*
-         * Процесс для вызов команды.
-         */
-        Process pr = null;
+	/**
+	 * Иницилизация runtime для того команды работали
+	 */
+	public Traceroute() throws Exception {
+		if (SystemUtils.IS_OS_WINDOWS == true)////для Windows tracert
+		{
+			OSType = 0;
+		} else if (SystemUtils.IS_OS_UNIX == true)//для linux
+		{
+			OSType = 1;
+		} else {
+			OSType = 3;
+			throw new Exception("Unknown operating system");
+		}
+		run = Runtime.getRuntime();
+	}
 
-        /*
-         * Выбор команды для конкрктной ОС. 
-         */
-        String cmd = getTracerouteCommand(destination);
+	/**
+	 * Вызов команду traceroute, и возврашает хопы в виде list->
+	 * TracerouteItems.
+	 *
+	 * @param destination пункт назначения hostname или IP
+	 * @return list of TracerouteItems
+	 */
+	public ArrayList<TracerouteItem> traceroute(String destination) {
+		/*
+		 * Это будет наш результат.
+		 */
+		ArrayList<TracerouteItem> result = new ArrayList<TracerouteItem>();
+		boolean firstLineFlag = true;
+		/*
+		 * Процесс для вызов команды.
+		 */
+		Process pr = null;
 
-        try {
-            pr = run.exec(cmd);
-        } catch (IOException e) {
-            /*
-             * Если что-то не так пойдет...
-             */
-            e.printStackTrace();
-        }
+		/*
+		 * Выбор команды для конкрктной ОС. 
+		 */
+		String cmd = getTracerouteCommand(destination);
 
-        /*
-         * Заготовка BufferedReader для четения вывод из traceroute.
-         */
-        BufferedReader buf = new BufferedReader(new InputStreamReader(
-                pr.getInputStream()));
+		try {
+			pr = run.exec(cmd);
+		} catch (IOException e) {
+			/*
+			 * Если что-то не так пойдет...
+			 */
+			e.printStackTrace();
+		}
 
-        /*
-         * Попытка разобрать каждую строку и создать соответствующий TracerouteItem
-         */
-        String line = "";
-        try {
-             while ((line = buf.readLine()) != null) {
-                line=new String(buf.readLine().getBytes(), Charset.forName("CP866"));
-                if (line.length() > 37) {                  
-                   if (firstLineFlag || "Tra".equals(line.substring(0, 3)) || "ove".equals(line.substring(0, 3)) || "Reque".equals(line.substring(32, 37))) 
-                    {
-                       firstLineFlag=false;
-                    } 
-                    else {
-                        firstLineFlag=false;
-                        if (OSType==0) //if windows
-                        {                           
-                            TracerouteItem item = parse(line);
-                            result.add(item);
-                        }
-                        else 
-                        {
-                            TracerouteItem item = parse(line);
-                            result.add(item);
-                        }   
-                    }
-                }
-            }
-        } catch (IOException e) {
-            /*
-             * А вдруг тут что-то не так :)
-             */
-            return null;
-        }
+		/*
+		 * Заготовка BufferedReader для четения вывод из traceroute.
+		 */
+		BufferedReader buf = new BufferedReader(new InputStreamReader(
+				pr.getInputStream()));
 
-        /*
-         * Конец метода.
-         */
-        return result;
-    }
+		/*
+		 * Попытка разобрать каждую строку и создать соответствующий TracerouteItem
+		 */
+		String line = "";
+		try {
+			while ((line = buf.readLine()) != null) {
+//				line = new String(buf.readLine().getBytes(), Charset.forName("CP866"));
+//                if (line.length() > 37) {                  
+//                   if (firstLineFlag || "Tra".equals(line.substring(0, 3)) || "ove".equals(line.substring(0, 3)) || "Reque".equals(line.substring(32, 37))) 
+//                    {
+//                       firstLineFlag=false;
+//                    } 
+//                    else {
+//                        firstLineFlag=false;
+//                        if (OSType==0) //if windows
+//                        {                           
+//                            TracerouteItem item = parse(line);
+//                            result.add(item);
+//                        }
+//                        else 
+//                        {
+//                            TracerouteItem item = parse(line);
+//                            result.add(item);
+//                        }   
+//                    }
+//                }
+				TracerouteItem item = parse(line);
+				result.add(item);
+			}
+		} catch (IOException e) {
+			/*
+			 * А вдруг тут что-то не так :)
+			 */
+			return null;
+		}
 
-    /**
-     * Извлекает необходимую информацию из строки вывода (один хоп) из
-     *          * Traceroute и делает TracerouteItem от него.
-     *
-     * @param line один хоп из traceroute
-     * @return the правильное TracerouteItem
-     */
-    public TracerouteItem parse(String line) {
-        String hostname = null;
-        String IP = null;
+		/*
+		 * Конец метода.
+		 */
+		return result;
+	}
 
-        int lenght = line.length();
-        int indexOf = line.indexOf("[");
+	/**
+	 * Извлекает необходимую информацию из строки вывода (один хоп) из
+	 *          * Traceroute и делает TracerouteItem от него.
+	 *
+	 * @param line один хоп из traceroute
+	 * @return the правильное TracerouteItem
+	 */
+	public TracerouteItem parse(String line) {
+//		System.out.println("line = " + line);
+		String hostname = null, IP = null;
+		Pattern hopPattern = Pattern.compile("^[ \t]*[0-9]+");
+		Matcher matcher = hopPattern.matcher(line);
+		if (matcher.find()) {
+			Pattern hostnamePattern = Pattern.compile("[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,6}"),
+					IPPattern = Pattern.compile("\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b");
+			matcher = hostnamePattern.matcher(line);
+			if (matcher.find()) {
+				hostname = matcher.group();
+			}
+			matcher = IPPattern.matcher(line);
+			if (matcher.find()) {
+				IP = matcher.group();
+			}
+		}
+//		System.out.println("hostname = " + hostname + "; IP = " + IP);
+		return new TracerouteItem(hostname, IP);
+	}
 
-        if (line.indexOf("[") == -1) {
-            IP = line.substring(32, lenght);
-        } else {
-            hostname = line.substring(32, indexOf - 1);
-            IP = line.substring(indexOf + 1, lenght - 2);
-        }
-        TracerouteItem tItem = new TracerouteItem(hostname, IP);
-        return tItem;
-    }
-
-    /**
-     * Получает надлежащую команду запустить трассировку для этой операционной
-     * системы.
-     *
-     * @param destination пункт назначения hostname или IP
-     * @return правильная traceroute команда
-     */
-    public String getTracerouteCommand(String destination) {
-        String cmd="";
-        if(OSType==0)////для Windows tracert
-        {
-             cmd = ("tracert " + destination);
-        } 
-        else if(OSType==1)//для linux
-        {
-             cmd = ("traceroute " + destination); 
-        };      
-        return cmd;
-    }
+	/**
+	 * Получает надлежащую команду запустить трассировку для этой операционной
+	 * системы.
+	 *
+	 * @param destination пункт назначения hostname или IP
+	 * @return правильная traceroute команда
+	 */
+	public String getTracerouteCommand(String destination) {
+		String cmd = "";
+		if (OSType == 0)////для Windows tracert
+		{
+			cmd = ("tracert " + destination);
+		} else if (OSType == 1)//для linux
+		{
+			cmd = ("traceroute " + destination);
+		};
+		return cmd;
+	}
 }
